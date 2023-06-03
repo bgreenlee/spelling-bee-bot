4ecbc8a527ae4a740e60cd3bbf70cfa67d0812f4
Brad Greenlee <brad@footle.org>
2023-06-03 16:45:50 -0700

    Add "missed" function to get words you missed yesterday

--- functions/post_words.ts
+++ functions/post_words.ts
@@ -129,6 +129,130 @@ async function postWords(
   }
 }
 
+// post the words you missed from yesterday
+async function missedWords(
+  client: SlackAPIClient,
+  user_id: string,
+  channel: string,
+  _timestamp: string,
+) {
+  // Querying datastore for stored token
+  const result = await client.apps.datastore.query({
+    datastore: DATASTORE_NAME,
+    expression: "user_id = :user_id",
+    expression_values: { ":user_id": user_id },
+  });
+  // console.log("got result:", result);
+  const items = result["items"];
+  if (items.length == 0) {
+    await client.chat.postMessage({
+      channel: channel,
+      text:
+        // "I couldn't find an auth token for you. Click <https://slack.com/shortcuts/Ft04N2S8EMDY/577bec46b867e18003fafe573f8015e5|here> to set one.",
+        "I couldn't find an auth token for you. Click <https://slack.com/shortcuts/Ft04NTFRR200/75386d12c12ef1d9bb9626f97bbe12ec|here> to set one.",
+    });
+  } else {
+    // fetch yesterday's word list (& game id)
+    let response = await fetch(
+      "https://www.nytimes.com/puzzles/spelling-bee",
+    );
+    if (!response.ok) {
+      return await {
+        error: `Error fetching game: ${response.statusText}`,
+        outputs: {},
+      };
+    }
+
+    const body = await response.text();
+    const match = body.match(
+      />window\.gameData = (\{.*?})<\/script>/,
+    );
+    if (!match) {
+      return await {
+        error: `Error parsing game data. Raw input: ${body}`,
+        outputs: {},
+      };
+    }
+
+    const gameData = JSON.parse(match[1]).yesterday;
+    const gameId = gameData.id;
+    const answers = gameData.answers;
+
+    // fetch the words
+    const auth_token = items[0]["nyt_token"];
+    response = await fetch(
+      `https://www.nytimes.com/svc/games/state/spelling_bee/latest?puzzle_id=${gameId}`,
+      {
+        headers: {
+          "nyt-s": auth_token,
+        },
+      },
+    );
+    if (!response.ok) {
+      console.log("Error fetching game data:", response.statusText);
+      return;
+    }
+
+    const data = await response.json();
+    const words = data.game_data.answers;
+
+    // figure out missing words
+    const missedWords = [];
+    for (const answer of answers) {
+      if (!words.includes(answer)) {
+        missedWords.push(answer);
+      }
+    }
+
+    missedWords.sort();
+    const formattedWords = missedWords.map((word: string) => {
+      // capitalize first letter
+      const capWord = word.charAt(0).toUpperCase() + word.slice(1);
+      // highlight pangrams
+      if (isPangram(word)) {
+        return `*${capWord}*`;
+      }
+      return capWord;
+    });
+
+    if (missedWords.length === 0) {
+      await client.chat.postMessage({
+        channel: channel,
+        text: `You didn't miss any words yesterday! :queen-bee:`,
+      });
+    } else {
+      await client.chat.postMessage({
+        channel: channel,
+        text: `Here are the words you missed yesterday...`,
+      });
+
+      // find the message to post to
+      let messageTs = null;
+      const messagesResp = await client.conversations.history({
+        channel: channel,
+      });
+      if (messagesResp.ok) {
+        for (const message of messagesResp["messages"]) {
+          if (
+            message.bot_id && message.text.indexOf("Here are the words") === 0
+          ) {
+            messageTs = message.ts;
+            break;
+          }
+        }
+      } else {
+        console.log("Error fetching message history:", messagesResp.error);
+      }
+
+      await client.chat.postMessage({
+        channel: channel,
+        thread_ts: messageTs,
+        text: formattedWords.join("\n"),
+      });
+    }
+  }
+}
+
 export default SlackFunction(
   PostWordsFunction,
   async ({ inputs, token }) => {
@@ -139,6 +263,15 @@ export default SlackFunction(
     switch (command.toLowerCase()) {
       case "words":
         await postWords(
+          client,
+          inputs.triggered_user,
+          inputs.channel,
+          inputs.timestamp,
+        );
+        break;
+      case "missed":
+      case "missing":
+        await missedWords(
           client,
           inputs.triggered_user,
           inputs.channel,
--- manifest.ts
+++ manifest.ts
@@ -1,17 +1,15 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
 import { AuthDatastore } from "./datastores/auth.ts";
-import { GamesDatastore } from "./datastores/games.ts";
 import { StoreAuthWorkflow } from "./workflows/store_auth.ts";
 import { PostWordsWorkflow } from "./workflows/post_words.ts";
-import { UpdateGamesWorkflow } from "./workflows/update_games.ts";
 
 export default Manifest({
   name: "Spelling Bee Bot",
   description: "NYT Spelling Bee Helper Bot",
   icon: "assets/icon.png",
-  workflows: [StoreAuthWorkflow, PostWordsWorkflow, UpdateGamesWorkflow],
+  workflows: [StoreAuthWorkflow, PostWordsWorkflow],
   outgoingDomains: ["www.nytimes.com", "edge.games.nyti.nyt.net"],
-  datastores: [AuthDatastore, GamesDatastore],
+  datastores: [AuthDatastore],
   botScopes: [
     "chat:write",
     "chat:write.public",
